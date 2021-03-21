a73910653e4be206a59296db05ac19972fd8fde3
Stefan <64359359+SteveMcT@users.noreply.github.com>
2021-03-21 02:40:50 +0100

    overwritten cursor

--- js/slider-hover.js
+++ js/slider-hover.js
@@ -1,11 +1,9 @@
 //Animation of Mobile 
-$(document).ready(() => {
-    $('#img-slider').mousemove(function(e) {
-        let x = e.pageX;
-        let y = e.pageY;
-        $('.img-slider-image').css('transform', 'rotateX(' + -y / 16000 + 'deg) rotateY(' + x / 16000 + 'deg)')
-    })
-    $('#img-slider').mouseleave(() => {
-        $('.img-slider-image').css('transform', 'rotate(0)')
-    });
+$('#img-slider').mousemove(function(e) {
+    let x = e.pageX;
+    let y = e.pageY;
+    $('.img-slider-image').css('transform', 'rotateX(' + -y / 16000 + 'deg) rotateY(' + x / 16000 + 'deg)')
 })
+$('#img-slider').mouseleave(() => {
+    $('.img-slider-image').css('transform', 'rotate(0)')
+});
